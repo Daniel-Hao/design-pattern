2de25916424e7b244f608952aca90a5820fa2eb0
Daniel-Hao <857897883@qq.com>
2017-01-05 11:43:23 +0800

    design

--- designpattern/app/src/main/java/com/daniel/www/designpattern/MainActivity.java
+++ designpattern/app/src/main/java/com/daniel/www/designpattern/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.daniel.www.factorymethod.common.SendFactory;
@@ -23,7 +24,6 @@ public class MainActivity extends AppCompatActivity implements DesignAdapter.OnI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         mRv = (RecyclerView) findViewById(R.id.rv);
 
         initData();
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity implements DesignAdapter.OnI
 
 
     }
-
 
     private void initData() {
         mDatas = new ArrayList<>();
@@ -68,17 +67,88 @@ public class MainActivity extends AppCompatActivity implements DesignAdapter.OnI
         mDatas.add("Interpreter 解释器模式");
     }
 
-
+    //region 私有方法
     private void factoryTest() {
         SendFactory factory = new SendFactory();
         Sender sender = factory.produce("sms");
         sender.send();
     }
 
+
+    //endregion
+
+    //region接口
     @Override
     public void onItemListener(int position, String text) {
+        if (TextUtils.isEmpty(text)) {
+            Toast.makeText(this, "点击数据为空", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        }
 
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        if (mDatas != null && mDatas.size() > 0) {
+            for (int i = 0; i < mDatas.size(); i++) {
+                if (mDatas.get(0).equals(text)) {
 
+                } else if (mDatas.get(1).equals(text)) {
+
+                } else if (mDatas.get(2).equals(text)) {
+
+                } else if (mDatas.get(3).equals(text)) {
+
+                } else if (mDatas.get(4).equals(text)) {
+
+                } else if (mDatas.get(5).equals(text)) {
+
+                } else if (mDatas.get(6).equals(text)) {
+
+                } else if (mDatas.get(7).equals(text)) {
+
+                } else if (mDatas.get(8).equals(text)) {
+
+                } else if (mDatas.get(9).equals(text)) {
+
+                } else if (mDatas.get(10).equals(text)) {
+
+                } else if (mDatas.get(11).equals(text)) {
+
+                } else if (mDatas.get(12).equals(text)) {
+
+                } else if (mDatas.get(13).equals(text)) {
+
+                } else if (mDatas.get(14).equals(text)) {
+
+                } else if (mDatas.get(15).equals(text)) {
+
+                } else if (mDatas.get(16).equals(text)) {
+
+                } else if (mDatas.get(17).equals(text)) {
+
+                } else if (mDatas.get(18).equals(text)) {
+
+                } else if (mDatas.get(19).equals(text)) {
+
+                } else if (mDatas.get(20).equals(text)) {
+
+                } else if (mDatas.get(21).equals(text)) {
+
+                } else if (mDatas.get(22).equals(text)) {
+
+                } else if (mDatas.get(23).equals(text)) {
+
+                } else if (mDatas.get(24).equals(text)) {
+
+                } else if (mDatas.get(25).equals(text)) {
+
+                } else if (mDatas.get(26).equals(text)) {
+
+                } else if (mDatas.get(27).equals(text)) {
+
+                }
+            }
+        }
     }
+    //endregion
+
 }
